f9ce1923e75222c87052c31ede764272567a4587
piny940 <an.s.shoma@gmail.com>
2023-11-22 22:21:44 +0900

    ExampleTNTBlocksを作成

---
+++ src/main/java/com/example/exampletnt/ExampleTNTBlocks.java
@@ -0,0 +1,19 @@
+package com.example.exampletnt;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ExampleTNTBlocks {
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(
+            ForgeRegistries.BLOCKS, ExampleTNT.MOD_ID);
+    public static final RegistryObject<Block> LARGE_TNT = BLOCKS.register(
+            "large_tnt",
+            () -> new Block(
+                    BlockBehaviour.Properties.of(
+                            Material.EXPLOSIVE).lightLevel(value -> 15)));
+
+}
