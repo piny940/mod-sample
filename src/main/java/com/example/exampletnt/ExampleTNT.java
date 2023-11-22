62269703432a8733bf09b084fa2eb10585eda3ab
piny940 <an.s.shoma@gmail.com>
2023-11-22 21:54:42 +0900

    texture関連のファイルは直接jsonに書き込む

--- src/main/java/com/example/exampletnt/ExampleTNT.java
+++ src/main/java/com/example/exampletnt/ExampleTNT.java
@@ -1,9 +1,5 @@
 package com.example.exampletnt;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,14 +10,6 @@ public class ExampleTNT {
 
     public ExampleTNT() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::registerProviders);
         ExampleTNTItems.register(modEventBus);
-    }
-
-    private void registerProviders(GatherDataEvent event) {
-        DataGenerator gen = event.getGenerator();
-        PackOutput packOutput = gen.getPackOutput();
-        ExistingFileHelper fileHelper = event.getExistingFileHelper();
-        gen.addProvider(event.includeClient(), new ExampleTNTItemModelProvider(packOutput, fileHelper));
     }
 }
